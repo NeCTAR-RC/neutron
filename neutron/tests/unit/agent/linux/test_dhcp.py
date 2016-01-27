c0963e3ed22739ec0b91157d31884325383ac6da
changzhi1990 <changzhi@unitedstack.com>
2016-01-27 11:22:34 +0800

    Fix bug when enable configuration named dnsmasq_base_log_dir
    DHCP agent will report an error when enable configuration
    named 'dnsmasq_base_log_dir'.
    
    For example, if this configuration is set to '/tmp'.
    The code checks dir '/tmp' if exists. This is wrong.
    The right way is to check '/tmp + [network_id]' existence.
    
    Change-Id: I0e060ca7c84f38bb0ccd55ac16da5446a3d015c5
    Closes-Bug: #1538386
    (cherry picked from commit 8ae32a6681ad9187e1394b9c633a6eb43df40342)

--- neutron/tests/unit/agent/linux/test_dhcp.py
+++ neutron/tests/unit/agent/linux/test_dhcp.py
@@ -1161,6 +1161,7 @@ class TestDnsmasq(TestBase):
                           '--log-dhcp',
                           ('--log-facility=%s' % dhcp_dns_log)],
                          network)
+        self.makedirs.assert_called_with(os.path.join('/tmp', network.id))
 
     def test_spawn_max_leases_is_smaller_than_cap(self):
         self._test_spawn(
