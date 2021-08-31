1c3ec334da7f272b1263f0297b11fb249886d036
Jake Yip <jake.yip@ardc.edu.au>
2021-08-31 15:50:22 +1000

    Nectarism: Disable removing of networksegements during sync
    When migrating with a fresh OVN database, there will be no chassis in
    OVN. This part of the code deletes segementhostmapping in Neutron when
    the chassis is not found, which breaks linuxbridge networks.
    
    The fix is to comment out the part of codes that handles nodes in
    Neutron but not in OVN. Update tests accordingly too.
    
    This may be reverted once all nodes that runs linuxbridge-agent also
    runs ovn-controller, which will creates their chassis in OVN.
    
    NOTE: This should not be removed if there are nodes that will only run
    linuxbridge and not ovn.
    
    Conflicts:
    	neutron/plugins/ml2/drivers/ovn/mech_driver/ovsdb/ovn_db_sync.py
    	neutron/tests/unit/plugins/ml2/drivers/ovn/mech_driver/ovsdb/test_ovn_db_sync.py
    
    Change-Id: I4f80a9f78c83c79c1c53bd5844e8b760de39d175
    (cherry picked from commit fe6033b5d6eaeb7df99d750309e35c3c73d80a85)
    (cherry picked from commit 4619f85d68901bfc8ae18b02c42fc1e4e53370f5)
    (cherry picked from commit 7b5c763a77b1de659c355d1e4b0f1c00422c3707)
    (cherry picked from commit 5d8c53663ad052e725204116bceeb819600751c4)
    (cherry picked from commit 6917a5f43936e611798986710a7ac861fe86db42)

--- neutron/tests/unit/plugins/ml2/drivers/ovn/mech_driver/ovsdb/test_ovn_db_sync.py
+++ neutron/tests/unit/plugins/ml2/drivers/ovn/mech_driver/ovsdb/test_ovn_db_sync.py
@@ -1230,15 +1230,11 @@ class TestOvnSbSyncML2(test_mech_driver.OVNMechanismDriverTestCase):
             mock_ghmws.assert_called_once_with(
                 mock.ANY,
                 include_agent_types=set(ovn_const.OVN_CONTROLLER_TYPES))
-            all_hosts = set(hostname_with_physnets.keys()) | hosts_in_neutron
             self.assertEqual(
-                len(all_hosts),
+                len(hostname_with_physnets.keys()),
                 ovn_driver.update_segment_host_mapping.call_count)
             update_segment_host_mapping_calls = [mock.call(
                 host, hostname_with_physnets[host])
                 for host in hostname_with_physnets]
-            update_segment_host_mapping_calls += [
-                mock.call(host, []) for host in
-                hosts_in_neutron - set(hostname_with_physnets.keys())]
             ovn_driver.update_segment_host_mapping.assert_has_calls(
                 update_segment_host_mapping_calls, any_order=True)
