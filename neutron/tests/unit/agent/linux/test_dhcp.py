d4f8bb258be90dd431cd6e95a72c88627ef86076
Sam Morrison <sorrison@gmail.com>
2022-11-03 15:59:06 +1100

    Nectar: Don't setup DHCP for midonet or geneve networks
    When using a single mech driver you wouldn't run dhcp-agent
    for ovn or midonet installs. We need dhcp agent for linuxbridge
    but don't want it managing ovn or midonet networks.
    
    This breaks metadata for midonet networks as it will create a
    DHCP port. Midonet sees this port of type network:dhcp and injects
    a metadata route which won't work midonet -> linuxbridge.
    
    We also don't want this for OVN networks as it creates a process
    and port which isn't used and not needed.
    
    Change-Id: I61da119635f72a6ecb379de33632f0db6603d56b

--- neutron/tests/unit/agent/linux/test_dhcp.py
+++ neutron/tests/unit/agent/linux/test_dhcp.py
@@ -671,6 +671,9 @@ class FakeV4Network(object):
         self.ports = [FakePort1()]
         self.namespace = 'qdhcp-ns'
 
+    def get(self, *args, **kwargs):
+        return None
+
 
 class FakeV4NetworkClientId(object):
     def __init__(self):
@@ -713,6 +716,9 @@ class FakeDualNetwork(object):
                       FakeDualPort(domain=domain),
                       FakeRouterHAPort(),
                       FakeRouterPort(domain=domain)]
+
+    def get(self, *args, **kwargs):
+        return None
 
 
 class FakeDeviceManagerNetwork(object):
