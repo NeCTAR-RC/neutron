8f566ce7610ca38ee7cd8de56d7c212b7187c340
Jake Yip <jake.yip@unimelb.edu.au>
2023-03-23 17:32:33 +1100

    Do not sync Southbound
    Leave Southbound alone at this point in time.
    
    sync_hostname_and_physical_networks() will conflict with linuxbridge
    [1].
    
    schedule_unhosted_gateways() will attempt to also assign midonet routers
    onto ovn chassis, so leave it alone.
    
    [1]: https://review.rc.nectar.org.au/c/NeCTAR-RC/networking-ovn/+/21789
    
    Change-Id: I4a15db878199350d378e8c91e904fa26f482aced
    (cherry picked from commit c748c99323a77e33c889240f078b84ea7c0f1582)
    (cherry picked from commit 804f7e780ecb1e1f053b8f743d8e6936a5bf5f7b)

--- neutron/plugins/ml2/drivers/ovn/mech_driver/ovsdb/ovn_db_sync.py
+++ neutron/plugins/ml2/drivers/ovn/mech_driver/ovsdb/ovn_db_sync.py
@@ -1368,10 +1368,10 @@ class OvnSbSynchronizer(OvnDbSynchronizer):
         """
         LOG.debug("Starting OVN-Southbound DB sync process")
 
-        ctx = context.get_admin_context()
-        self.sync_hostname_and_physical_networks(ctx)
-        if utils.is_ovn_l3(self.l3_plugin):
-            self.l3_plugin.schedule_unhosted_gateways()
+        # ctx = context.get_admin_context()
+        # self.sync_hostname_and_physical_networks(ctx)
+        # if utils.is_ovn_l3(self.l3_plugin):
+        #     self.l3_plugin.schedule_unhosted_gateways()
         # NOTE(ralonsoh): this could be called using a resource event.
         self.ovn_driver._ovn_client.placement_extension.\
             read_initial_chassis_config()
