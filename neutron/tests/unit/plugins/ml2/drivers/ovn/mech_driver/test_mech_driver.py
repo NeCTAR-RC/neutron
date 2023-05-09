9953bf5e321096f8f8b61a8a43c40bb53bb70a1a
Lucas Alvares Gomes <lucasagomes@gmail.com>
2023-05-09 13:36:54 +0100

    [OVN] Expose chassis hosting information in LSP
    Expose chassis hosting information via LSP's external_ids. This allows
    for projects such as ovn-bgp-agent NB driver that only connects to the
    NB database to consume such information (avoiding more connections to
    the SB database).
    
    Also stop populating the "requested-chassis" options for ports of the
    type virtual because it's ignored by OVN.
    
    See LP #2020058 for more information.
    
    Conflicts:
      neutron/plugins/ml2/drivers/ovn/mech_driver/ovsdb/ovn_client.py
    
    Closes-Bug: #2020058
    Change-Id: I1fb6a9ff94376ab5f695d311b6fa8034d73cf556
    Signed-off-by: Lucas Alvares Gomes <lucasagomes@gmail.com>
    (cherry picked from commit 28926957d691c8b0897c71fee59a888783d15974)
    (cherry picked from commit 3e6b2b77b2aa84260864c53bf7bcdd7611a848ef)

--- neutron/tests/unit/plugins/ml2/drivers/ovn/mech_driver/test_mech_driver.py
+++ neutron/tests/unit/plugins/ml2/drivers/ovn/mech_driver/test_mech_driver.py
@@ -1064,7 +1064,9 @@ class TestOVNMechanismDriver(TestOVNMechanismDriverBase):
                 mock.patch.object(self.mech_driver,
                                   '_update_dnat_entry_if_needed') as ude, \
                 mock.patch.object(self.mech_driver, '_should_notify_nova',
-                                  return_value=is_compute_port):
+                                  return_value=is_compute_port), \
+                mock.patch.object(self.mech_driver._ovn_client,
+                                  'update_lsp_host_info') as ulsp:
             self.mech_driver.set_port_status_up(port1['port']['id'])
             pc.assert_called_once_with(
                 mock.ANY,
@@ -1083,6 +1085,8 @@ class TestOVNMechanismDriver(TestOVNMechanismDriverBase):
                 self.mech_driver._plugin.nova_notifier.\
                     notify_port_active_direct.assert_called_once_with(
                         mock.ANY)
+
+            ulsp.assert_called_once_with(mock.ANY, mock.ANY)
 
     def test_set_port_status_up(self):
         self._test_set_port_status_up(is_compute_port=False)
@@ -1103,7 +1107,9 @@ class TestOVNMechanismDriver(TestOVNMechanismDriverBase):
                 mock.patch.object(self.mech_driver,
                                   '_update_dnat_entry_if_needed') as ude, \
                 mock.patch.object(self.mech_driver, '_should_notify_nova',
-                                  return_value=is_compute_port):
+                                  return_value=is_compute_port), \
+                mock.patch.object(self.mech_driver._ovn_client,
+                                  'update_lsp_host_info') as ulsp:
             self.mech_driver.set_port_status_down(port1['port']['id'])
             apc.assert_called_once_with(
                 mock.ANY,
@@ -1128,6 +1134,8 @@ class TestOVNMechanismDriver(TestOVNMechanismDriverBase):
                 self.mech_driver._plugin.nova_notifier.\
                     send_port_status.assert_called_once_with(
                         None, None, mock.ANY)
+
+            ulsp.assert_called_once_with(mock.ANY, mock.ANY, up=False)
 
     def test_set_port_status_down(self):
         self._test_set_port_status_down(is_compute_port=False)
